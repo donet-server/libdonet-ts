43180f9aa3d3f7d69fc3ed9d51704251bd61b027
Max Rodriguez <max@max-rodriguez.com>
2022-06-23 22:51:10 -0700

    parser_test: Updated ParserTest.ts test file

--- tests/ParserTest.ts
+++ tests/ParserTest.ts
@@ -4,7 +4,13 @@ import { STATUS, MODULE_DEBUG_FLAGS } from './../build/globals'
 MODULE_DEBUG_FLAGS.DC_PARSER = true
 
 const parser = new Astron.DCParser()
-const dcFile1 = parser.parse_file('parser_test.dc')
 
-if (dcFile1 === STATUS.FAILURE) process.exit(1)
-//console.dir(dcFile1, {depth: null})
+// Run full test DC file (3k lines)
+const testDC = parser.parse_file('parser_test.dc')
+if (testDC === STATUS.FAILURE) process.exit(1)
+console.log("\n\n\n\n")
+
+// Run simple DC file (less overwhelming to observe)
+const simpleDC = parser.parse_file('simple_example.dc')
+if (simpleDC === STATUS.FAILURE) process.exit(1)
+console.dir(simpleDC, {depth: null})
