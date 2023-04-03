8a03909df32ba3292a1e685b77fb5f42f4deec50
Max Rodriguez <max@vulkray.com>
2023-04-03 16:11:45 -0700

    connection: Read to data buffer & wrote poll_datagram() method

--- src/node/Connection.ts
+++ src/node/Connection.ts
@@ -8,10 +8,12 @@
 */
 
 import { MODULE_DEBUG_FLAGS, MD_PORT } from './globals'
+import { Datagram } from './Datagram'
 import * as error from './Errors'
-import * as buffer from 'node:buffer'
+import { Buffer } from 'node:buffer'
 import * as net from 'node:net'
-import * as os from 'node:os'
+
+const DATAGRAM_HEADER_SIZE: number = 2 // bytes
 
 export class Connection {
     private __DEBUG__: boolean = MODULE_DEBUG_FLAGS.CONNECTION
@@ -19,42 +21,59 @@ export class Connection {
     private readonly port: number
     private connected: boolean = false
     private socket: net.Socket
+    private data_buffer: Buffer = Buffer.alloc(0)
+    private receiving: boolean = false
 
     constructor(host: string = "127.0.0.1", port: number = MD_PORT) {
         this.host = host; this.port = port;
         this.socket = new net.Socket()
-        this.socket.on('connect', this.on_connect)
-        this.socket.on('data', this.read_data)
-        this.socket.on('error', this.socket_error)
+
+        this.socket.on('connect', () => {
+            this.connected = true
+            this.notify("TCP socket connected!")
+        })
+        this.socket.on('data', (data: Buffer) => {
+            if (data.length > 0) // add bytes received to the data buffer
+                this.data_buffer = Buffer.concat([this.data_buffer, data])
+
+            if (this.data_buffer.length > 0) return
+            // check if we still have more to add to our data buffer
+            let dg_size: number = this.data_buffer.readUint16LE(0)
+            this.receiving = (this.data_buffer.length - DATAGRAM_HEADER_SIZE) < dg_size
+        })
+        this.socket.on('error', (err: Error) => {
+            if (err.message.includes('ECONNREFUSED'))
+                throw new error.AstronConnectionRefused // throw our own error message
+            else
+                throw err // not handled, throw it
+        })
         this.socket.connect({port: this.port, host: this.host})
     }
 
-    private notify(msg: string): void {
-        if (!this.__DEBUG__) return
-        console.log(`${this.constructor.name}: ${msg}`)
-    }
-
-    private socket_error(err: Error): void {
-        if (err.message.includes('ECONNREFUSED'))
-            throw new error.AstronConnectionRefused // throw our own error message
-        else
-            throw err // not handled, throw it
-    }
-
-    private on_connect(): void {
-        return
-    }
-
-    private read_data(data: buffer.Buffer): void {
-        // TODO: handle reading data, use Datagram
+    public poll_datagram(): Datagram | void {
+        /*  If the socket has finished receiving all the data,
+            it will be stored into a Datagram object and returned.
+            If we're still receiving data, this method returns void.
+        */
+        if (!this.receiving) return
+        let dg: Datagram = new Datagram()
+        dg.add_data([this.data_buffer])
+        this.data_buffer = Buffer.alloc(0) // empty buffer
+        this.receiving = false
+        return dg
     }
 
     public disconnect(): void {
         this.socket.destroy()
         this.connected = false
-        this.notify("TCP socket closed!")
+        this.notify("TCP socket connection closed!")
     }
     public is_connected(): boolean {
         return this.connected
+    }
+
+    private notify(msg: string): void {
+        if (!this.__DEBUG__) return
+        console.log(`${this.constructor.name}: ${msg}`)
     }
 }
